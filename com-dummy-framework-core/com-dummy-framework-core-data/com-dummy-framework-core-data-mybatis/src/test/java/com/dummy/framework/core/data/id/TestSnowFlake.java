b0c713165a9852872a060b12a16236b604dfd97f
kanghouchao <hchkang8710@gmail.com>
2019-04-22 22:46:44 +0800

    加入雪花算法依赖包和单元测试类

---
+++ com-dummy-framework-core/com-dummy-framework-core-data/com-dummy-framework-core-data-mybatis/src/test/java/com/dummy/framework/core/data/id/TestSnowFlake.java
@@ -0,0 +1,39 @@
+package com.dummy.framework.core.data.id;
+
+import org.junit.Test;
+import xyz.downgoon.snowflake.Snowflake;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author Vincent Kang
+ * @date 19-4-21
+ **/
+public class TestSnowFlake {
+
+    @Test
+    public void testGetId() {
+        Snowflake snowflake = new Snowflake(2, 5);
+        Long id = snowflake.nextId();
+        System.out.println(String.format("TestSnowFlake.testGetId,id is %d", id));
+    }
+
+    @Test
+    public void testGetIdA() throws InterruptedException {
+        final Snowflake snowflake = new Snowflake(2, 5);
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        final CountDownLatch countDownLatch = new CountDownLatch(10000);
+        for (int i = 0; i < 10000; i++) {
+            service.execute(() -> {
+                        System.out.println(String.format("TestSnowFlake.testGetId,time is %d, count is %d, id is %d", System.currentTimeMillis(), countDownLatch.getCount(), snowflake.nextId()));
+                        countDownLatch.countDown();
+                    }
+            );
+        }
+        countDownLatch.await();
+        service.shutdown();
+    }
+
+}
