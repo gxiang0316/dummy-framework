363da067d39b131ac7a7845d96bab93fd2d39843
kanghouchao <hchkang8710@gmail.com>
2020-07-26 12:39:27 +0800

    扩展请求参数处理，从请求头中获取客户的类别

---
+++ dummy-expand/spring-web/src/main/java/com/dummy/framework/web/method/support/DeviceHandlerMethodArgumentResolver.java
@@ -0,0 +1,55 @@
+/*
+  Copyright 2019 kanghouchao
+  <p>
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  <p>
+  http://www.apache.org/licenses/LICENSE-2.0
+  <p>
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+ */
+
+package com.dummy.framework.web.method.support;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Get device info from request Head
+ *
+ * @author Lurker
+ * @since 2020/07/24
+ */
+public class DeviceHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return true;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+        Assert.notNull(request, "Can't find HttpServletRequest!");
+        final String REQUEST_HEADER_DEVICE_KEY = "x-device-id";
+        String id = request.getHeader(REQUEST_HEADER_DEVICE_KEY);
+        if (id != null) {
+            DeviceInfo deviceInfo = new DeviceInfo();
+            deviceInfo.setId(id);
+            return deviceInfo;
+        }
+        return null;
+    }
+
+}
